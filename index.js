23934cd7194f8f86a9a9acd8f6e348c05dd85a2c
surinder singh <surinder83singh@gmail.com>
2020-05-12 00:43:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const utils = require("./lib/utils");
-const Router = require("./lib/router");
 const App = require("./lib/app");
-const RPC = require("./lib/rpc");
 
-module.exports = {
-	utils,
-	App, Router, RPC, 
-	FlowApp:App, FlowRouter:Router, FlowRPC:RPC
-}
+module.exports = App;
